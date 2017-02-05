77e4b9fdd6c25b44dd953865d8789dc2248c7f1f
kategld <tiglada@yandex.ru>
2017-02-05 10:55:05 +0700

    Упрощена компонента information

--- src/app/components/information/information.component.js
+++ src/app/components/information/information.component.js
@@ -1,11 +1,5 @@
 import InformationCtrl from './information.controller';
 
 export default {
-    bindings: {
-        text: '=',
-        pictureURL:'='
-    },
     templateUrl: './components/information/information.html',
-    controller: InformationCtrl,
-    controllerAs: '$ctrl'
 };
--- src/app/components/information/information.controller.js
+++
@@ -1,5 +0,0 @@
-class InformationCtrl {
-    constructor() {}
-
-
-export default InformationCtrl;
