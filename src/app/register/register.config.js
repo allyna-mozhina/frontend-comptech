afb35aabd1f38fe96184ac659813f2983a5f2491
dianavasilyeva <vasdianavas@gmail.com>
2017-02-05 20:27:52 +0600

    Прикрутили бустрап стили к регистрационной странице

--- src/app/register/register.config.js
+++ src/app/register/register.config.js
@@ -7,9 +7,7 @@ function RegisterConfig($stateProvider) {
             templateUrl: './register/register.html',
             controller: 'RegisterCtrl',
             controllerAs: '$ctrl',
-            resolve: {
-
-            }
+            resolve: {}
         });
 }
 
--- src/app/register/register.controller.js
+++ src/app/register/register.controller.js
@@ -2,6 +2,16 @@ class RegisterCtrl {
     constructor() {
         'ngInject';
 
+        // Login Form
+        this.registerFormData = {};
+        this.registerFormErrors = [];
+    }
+    
+    register() {
+        console.log('send', this.registerFormData);
+        // TODO Тут обращаемся к сервису, в промисе получаем ответ и реализуем нужную логику
+        this.registerFormErrors.push('Ошибочка вышла');
+        this.registerFormErrors.push('Что-то пошло не так!');
     }
 }
 
