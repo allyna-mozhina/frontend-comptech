58fd9eb1e7fec7cb27bde16597e5c1d9e728a5d1
dianavasilyeva <vasdianavas@gmail.com>
2017-02-06 16:57:34 +0600

    Добавлена поддержка авторизации в PulseService

--- src/app/config/app.constants.js
+++ src/app/config/app.constants.js
@@ -1,7 +1,8 @@
 const AppConstants = {
     API_URL: 'https://private-a1460-example225.apiary-mock.com',
     TOKEN_KEY: 'auth_token',
-    GRANT_TYPE: 'password'
+    GRANT_TYPE: 'password',
+    TOKEN_TYPE: 'Bearer'
 };
 
 export default AppConstants;
--- src/app/services/pulse.service.js
+++ src/app/services/pulse.service.js
@@ -1,15 +1,19 @@
 export default class PulseService {
-    constructor($http, AppConstants) {
+    constructor($http, Auth, AppConstants) {
         'ngInject';
 
         this._$http = $http;
         this._AppConstants = AppConstants;
+        this._Auth = Auth;
     }
 
     get() {
-        return this._$http.get(`${this._AppConstants.API_URL}/pulse/`);
+        return this._$http({
+            url: `${this._AppConstants.API_URL}/rest/pulse`,
+            method: 'GET',
+            headers: {
+                'Authorization': this._AppConstants.TOKEN_TYPE + ' ' +  this._Auth.getToken()
+            }
+        });
     }
-
-
-
 }
