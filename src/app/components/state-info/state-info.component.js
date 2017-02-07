5cac57008f7346461339c95a7b94a692d019dfa8
Алина Можина <mohinalina@gmail.com>
2017-02-07 13:39:28 +0700

    Настроено отображение информации о статусе тренировки

--- src/app/components/state-info/state-info.component.js
+++ src/app/components/state-info/state-info.component.js
@@ -2,7 +2,8 @@ import StateInfoCtrl from './state-info.controller';
 
 export default {
     bindings: {
-        pulse: '='
+        pulse: '=',
+        isTraining: '='
     },
     templateUrl: './components/state-info/state-info.html',
     controller: StateInfoCtrl,
