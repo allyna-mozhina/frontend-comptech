544d4bab3f324d60fd260ea341f8596097963b71
Atures <nrk.atures@gmail.com>
2017-02-07 12:54:47 +0700

    Отлажена анимация сердца в компоненте state-form

--- src/app/config/app.constants.js
+++ src/app/config/app.constants.js
@@ -1,5 +1,6 @@
 const AppConstants = {
-    API_URL: 'https://private-a1460-example225.apiary-mock.com',
+    //API_URL: 'https://private-a1460-example225.apiary-mock.com',
+    API_URL: 'http://private-36fbc-comptech.apiary-mock.com',
     TOKEN_KEY: 'auth_token',
     GRANT_TYPE: 'password',
     TOKEN_TYPE: 'Bearer'
