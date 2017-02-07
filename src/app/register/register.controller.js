a00f37a81af0465851d0eb9162063fc04353328a
Алина Можина <mohinalina@gmail.com>
2017-02-07 13:05:53 +0700

    Добавлена логика регистрации с последующим входом

--- src/app/register/register.controller.js
+++ src/app/register/register.controller.js
@@ -1,6 +1,12 @@
+import _ from 'lodash';
+
 class RegisterCtrl {
-    constructor() {
+    constructor($state, Auth, AppConstants) {
         'ngInject';
+
+        this._$state = $state;
+        this._Auth = Auth;
+        this._AppConstants = AppConstants;
 
         // Login Form
         this.registerFormData = {};
@@ -8,10 +14,28 @@ class RegisterCtrl {
     }
     
     register() {
-        console.log('send', this.registerFormData);
-        // TODO Тут обращаемся к сервису, в промисе получаем ответ и реализуем нужную логику
-        this.registerFormErrors.push('Ошибочка вышла');
-        this.registerFormErrors.push('Что-то пошло не так!');
+        this.registerFormData['grant_type'] = this._AppConstants.GRANT_TYPE;
+        this.registerFormData['username'] = this.registerFormData.userName;
+
+        const REG_FIELDS = ['userName', 'password'];
+        const regData = _.pick(this.registerFormData, REG_FIELDS);
+
+        const AUTH_FIELDS = ['username', 'password', 'grant_type'];
+        const authData = _.pick(this.registerFormData, AUTH_FIELDS);
+
+        console.log('send', regData);
+
+        return this._Auth.register(regData)
+            .then(
+                () => this._Auth.login(authData),
+                (error) => this.registerFormErrors.push(error))
+            .then(
+                (resp) => {
+                    this._Auth.setToken(resp.data['access_token'], false);
+                    this._$state.go('app.training')
+                },
+                (error) => this.registerFormErrors.push(error)
+            );
     }
 }
 
